62f9e43069cbe600381fec746967501aa058d1fe
dwibambang72 <72191762+dwibambang72@users.noreply.github.com>
2022-12-14 17:20:10 +0700

    Create Button.jsx

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+
+
+import React from "react";
+import "./button.css";
+
+const STYLES = [
+  "btn--primary--solid",
+  "btn--warning--solid",
+  "btn--danger--solid",
+  "btn--success--solid",
+  "btn--primary--solid",
+
+]
