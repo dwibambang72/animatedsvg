bd8016c82e5107c1e9dacbcb91f001f9b26467c4
dwibambang72 <72191762+dwibambang72@users.noreply.github.com>
2022-12-14 13:57:07 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { ReactComponent as TextSVG } from "./assets/undraw_text.svg";
+
+export default function App() {
+  return (
+    <div className="App">
+      <TextSVG />
+    </div>
+  );
+}
